f8e9d838bc31eedd7630a843955af55cdd43612f
choihyejin <sorate_@naver.com>
2023-08-23 10:33:20 +0900

    Bubble로 파일명 변경

---
+++ app/Bubble.tsx
@@ -0,0 +1,22 @@
+export default function BubbleEffect() {
+  return (
+    <div className="bubble-area">
+      <div className="left">
+        <div className="small"></div>
+        <div className="medium"></div>
+        <div className="s-medium"></div>
+        <div className="medium"></div>
+        <div className="large"></div>
+        <div className="small-l"></div>
+      </div>
+      <div className="right">
+        <div className="small"></div>
+        <div className="medium"></div>
+        <div className="s-medium"></div>
+        <div className="medium"></div>
+        <div className="large"></div>
+        <div className="small-l"></div>
+      </div>
+    </div>
+  );
+}
